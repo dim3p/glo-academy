fb8b399cce9c351b829b205d1bfa6ccdeec3c1b5
Dmitry Panteleev <dim3p@yandex.ru>
2024-10-27 23:29:12 +0300

    lesson01 compleat

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// lesson01
+// В задании написано создать переменные, про их инициализации не сказанно
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+alert("Hello world!");
 console.log("Hello world!");
