bab79bbb0bc809af6b385188afead962b8dfc13a
Dmitry Panteleev <dim3p@yandex.ru>
2024-10-28 23:46:47 +0300

    lesson02+ compleat

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-// lesson02
-// Step 1
-let title = 'Lesson02';
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 100500;
-let rollback = 50;
-let fullPrice = 350000;
-let adaptive = true;
+// lesson02+
 
-// Step 2
-console.log("Type title:", typeof title);
-console.log("Type fullPrice:", typeof fullPrice);
-console.log("Type adaptive:", typeof adaptive);
-
-console.log("length of variable 'screens':", screens.length);
-
-console.log("Стоимость верстки экранов", screenPrice, "рублей/долларов/гривен/юани");
-console.log("Стоимость разработки сайта", fullPrice, "рублей/долларов/гривен/юани");
-
-console.log(screens.toLowerCase().split(/\s*,\s*/)); // разделитель регулярное выражение
-
-console.log("Процент отката посреднику за работу:", fullPrice * (rollback / 100));
+let num = 266219;
+num = String(num).split('');
+let res = 1;
+for (i in num) { res = res * Number(num[i]); }
+res = res ** 3;
+console.log(String(res).substring(0, 2));
