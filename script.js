5c278e2c03ad227cb4b605aafdef432fcc4870ad
Dmitry Panteleev <dim3p@yandex.ru>
2024-10-28 23:07:42 +0300

    lesson02 compleat

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-// lesson01
-// В задании написано создать переменные, про их инициализации не сказанно
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
-alert("Hello world!");
-console.log("Hello world!");
+// lesson02
+// Step 1
+let title = 'Lesson02';
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 100500;
+let rollback = 50;
+let fullPrice = 350000;
+let adaptive = true;
+
+// Step 2
+console.log("Type title:", typeof title);
+console.log("Type fullPrice:", typeof fullPrice);
+console.log("Type adaptive:", typeof adaptive);
+
+console.log("length of variable 'screens':", screens.length);
+
+console.log("Стоимость верстки экранов", screenPrice, "рублей/долларов/гривен/юани");
+console.log("Стоимость разработки сайта", fullPrice, "рублей/долларов/гривен/юани");
+
+console.log(screens.toLowerCase().split(/\s*,\s*/)); // разделитель регулярное выражение
+
+console.log("Процент отката посреднику за работу:", fullPrice * (rollback / 100));
